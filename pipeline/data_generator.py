4cf9348f0411dfd174cf51a1bd656eef22bf1ced
SUBHANKAR-wq <mishrasubhankar01@gmail.com>
2026-02-14 22:46:30 +0530

    change in pipe line

--- pipeline/data_generator.py
+++ pipeline/data_generator.py
@@ -4,16 +4,34 @@ from datetime import datetime
 
 def data_generator(data):
     """
-    Convert raw sensor list into structured format
+    Convert raw sensor input into structured dataframe
+
+    Supports:
+    - list  -> [temp, pressure, flow, vibration]
+    - dict  -> {"temperature":..., ...}
     """
 
-    features = {
-        "timestamp": datetime.now(),
-        "temperature": float(data[0]),
-        "pressure": float(data[1]),
-        "flow_rate": float(data[2]),
-        "vibration": float(data[3]),
-    }
+    # ===== IF INPUT IS DICTIONARY =====
+    if isinstance(data, dict):
+
+        features = {
+            "timestamp": datetime.now(),
+            "temperature": float(data["temperature"]),
+            "pressure": float(data["pressure"]),
+            "flow_rate": float(data["flow_rate"]),
+            "vibration": float(data["vibration"]),
+        }
+
+    # ===== IF INPUT IS LIST =====
+    else:
+
+        features = {
+            "timestamp": datetime.now(),
+            "temperature": float(data[0]),
+            "pressure": float(data[1]),
+            "flow_rate": float(data[2]),
+            "vibration": float(data[3]),
+        }
 
     df = pd.DataFrame([features])
 
