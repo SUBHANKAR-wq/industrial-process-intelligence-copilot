3008733f41411d3c9997726a09b90d31574c4c48
SUBHANKAR-wq <mishrasubhankar01@gmail.com>
2026-02-14 14:24:26 +0530

    Added CORS and health endpoint

--- api/main.py
+++ api/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router
 
 app = FastAPI(
     title="Industrial Process Intelligence API"
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # later you can restrict to React URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# API routes
 app.include_router(router)
+
+
+# health check endpoint (very useful)
+@app.get("/")
+def root():
+    return {"status": "API running"}
