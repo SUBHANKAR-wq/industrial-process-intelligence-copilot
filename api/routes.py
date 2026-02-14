a2c203677cc721f9ae8f769e1d4da9d35c46eeff
SUBHANKAR-wq <mishrasubhankar01@gmail.com>
2026-02-14 18:47:46 +0530

    added error contol

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pipeline.main_pipeline import main_pipeline
+import traceback
 
 router = APIRouter()
 
@@ -9,9 +10,14 @@ def predict(data: dict):
     """
     Run full pipeline
     """
+    try:
+        sensor_values = data["sensor_values"]
 
-    sensor_values = data["sensor_values"]
+        result = main_pipeline(sensor_values)
 
-    result = main_pipeline(sensor_values)
+        return result
 
-    return result
+    except Exception as e:
+        print("ERROR OCCURRED:", e)
+        traceback.print_exc()
+        return {"error": str(e)}
